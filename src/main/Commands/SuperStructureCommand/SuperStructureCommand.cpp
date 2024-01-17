67749556e0cbbfc6b8ddfd13449454ba0e2210d3
Luis Leyva <luisoleyvan@gmail.com>
2024-01-17 17:41:25 -0600

    Wrong Subsystem required fix

--- src/main/Commands/SuperStructureCommand/SuperStructureCommand.cpp
+++ src/main/Commands/SuperStructureCommand/SuperStructureCommand.cpp
@@ -3,13 +3,13 @@
 frc2::CommandPtr StartIntake(Intake* m_Intake, SuperStructure* m_SuperStructure) {
 	return frc2::cmd::Parallel(
 		frc2::InstantCommand([m_Intake] {m_Intake->setVoltage(3_V);}, { m_Intake }).ToPtr(),
-		frc2::InstantCommand([m_SuperStructure] {m_SuperStructure->setTargetCoord({ 30, -15 });}, { m_Intake }).ToPtr()
+		frc2::InstantCommand([m_SuperStructure] {m_SuperStructure->setTargetCoord({ 30, -15 });}, { m_SuperStructure }).ToPtr()
 	);
 }
 
 frc2::CommandPtr StopIntake(Intake* m_Intake, SuperStructure* m_SuperStructure) {
 	return frc2::cmd::Parallel(
 		frc2::InstantCommand([m_Intake] {m_Intake->setVoltage(0_V);}, { m_Intake }).ToPtr(),
-		frc2::InstantCommand([m_SuperStructure] {m_SuperStructure->setTargetCoord({ 0, 0 });}, { m_Intake }).ToPtr()
+		frc2::InstantCommand([m_SuperStructure] {m_SuperStructure->setTargetCoord({ 0, 0 });}, { m_SuperStructure }).ToPtr()
 	);
 }
