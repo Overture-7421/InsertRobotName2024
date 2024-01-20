3c508865767aec7fa5016adae13cb8fa136c4d1e
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2024-01-19 20:58:00 -0600

    New robot simulation model

--- src/main/Robot.cpp
+++ src/main/Robot.cpp
@@ -9,43 +9,43 @@
 void Robot::RobotInit() {
 #ifndef __FRC_ROBORIO__
   simMotorManager->Init({
-    {1, "FirstRobotConcept/motors/SDS_Module_FL_rotation_joint"}, 
-    {2, "FirstRobotConcept/motors/SDS_Module_FL_wheel_joint"},
+    {1, "7421-24/motors/SDS_Module_FL_rotation_joint"}, 
+    {2, "7421-24/motors/SDS_Module_FL_wheel_joint"},
     
-    {3, "FirstRobotConcept/motors/SDS_Module_FR_rotation_joint"}, 
-    {4, "FirstRobotConcept/motors/SDS_Module_FR_wheel_joint"},
+    {3, "7421-24/motors/SDS_Module_FR_rotation_joint"}, 
+    {4, "7421-24/motors/SDS_Module_FR_wheel_joint"},
 
-    {5, "FirstRobotConcept/motors/SDS_Module_BR_rotation_joint"}, 
-    {6, "FirstRobotConcept/motors/SDS_Module_BR_wheel_joint"},
+    {5, "7421-24/motors/SDS_Module_BR_rotation_joint"}, 
+    {6, "7421-24/motors/SDS_Module_BR_wheel_joint"},
 
-    {7, "FirstRobotConcept/motors/SDS_Module_BL_rotation_joint"}, 
-    {8, "FirstRobotConcept/motors/SDS_Module_BL_wheel_joint"},
+    {7, "7421-24/motors/SDS_Module_BL_rotation_joint"}, 
+    {8, "7421-24/motors/SDS_Module_BL_wheel_joint"},
 
-    {20, "FirstRobotConcept/motors/arm_joint_right"},
-    {21, "FirstRobotConcept/motors/arm_joint_left"},
-    {22, "FirstRobotConcept/motors/shooter_arm_joint"},
-    {23, "FirstRobotConcept/motors/supports_joint_up"},
+    {20, "7421-24/motors/chassis_arm_joint_right"},
+    {21, "7421-24/motors/chassis_arm_joint_left"},
+    {22, "7421-24/motors/arm_shooter_joint"},
+    {23, "7421-24/motors/chassis_supports_joint"},
 
-    {24, "FirstRobotConcept/motors/intake_wheels_joint"}, // Testing Storage Code
-
-    // {25, "FirstRobotConcept/motors/intake_wheels_joint"},
-    {26, "FirstRobotConcept/motors/shooter_roller_joint"}
+    {24, "7421-24/motors/shooter_storage_wheel_left_joint"},
+    {25, "7421-24/motors/chassis_intake_wheels_joint"},
+    {26, "7421-24/motors/shooter_roller_up_joint_up"}
+    {27, "7421-24/motors/shooter_roller_up_joint_down"}
 
     });
 
-  simPigeonManager->Init("FirstRobotConcept/chassis/imu_sensor");
+  simPigeonManager->Init("7421-24/chassis/imu_sensor");
 
   simCANCoderManager->Init({
-    {9,  "FirstRobotConcept/cancoders/SDS_Module_FL_rotation_joint"}, 
-    {10, "FirstRobotConcept/cancoders/SDS_Module_FR_rotation_joint"},
-    {11, "FirstRobotConcept/cancoders/SDS_Module_BR_rotation_joint"},
-    {12, "FirstRobotConcept/cancoders/SDS_Module_BL_rotation_joint"},
+    {9,  "7421-24/cancoders/SDS_Module_FL_rotation_joint"}, 
+    {10, "7421-24/cancoders/SDS_Module_FR_rotation_joint"},
+    {11, "7421-24/cancoders/SDS_Module_BR_rotation_joint"},
+    {12, "7421-24/cancoders/SDS_Module_BL_rotation_joint"},
     });
 
   simDutyCycleEncoderManager->Init({
-    {0, "FirstRobotConcept/cancoders/arm_joint"}, 
-    {1, "FirstRobotConcept/cancoders/shooter_arm_joint"},
-    {2, "FirstRobotConcept/cancoders/supports_joint"},
+    {0, "7421-24/cancoders/chassis_arm_joint"}, 
+    {1, "7421-24/cancoders/arm_shooter_joint"},
+    {2, "7421-24/cancoders/chassis_supports_joint"},
   });
 
 #endif
