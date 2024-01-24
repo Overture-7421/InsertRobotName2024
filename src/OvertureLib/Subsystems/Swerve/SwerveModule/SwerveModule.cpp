a7caf7f93cd90004d9e6982b9dfc27f7d949632c
Luis Leyva <luisoleyvan@gmail.com>
2024-01-24 09:31:11 -0600

    Commented some shuffleboard numbers

--- src/OvertureLib/Subsystems/Swerve/SwerveModule/SwerveModule.cpp
+++ src/OvertureLib/Subsystems/Swerve/SwerveModule/SwerveModule.cpp
@@ -184,11 +184,7 @@ void SwerveModule::setVoltages() {
 }
 
 void SwerveModule::Periodic() {
-	frc::SmartDashboard::PutNumber(m_name + "/Speed", getSpeed());
-	#ifndef __FRC_ROBORIO__
-	frc::SmartDashboard::PutNumber(m_name + "/Target", m_state.angle.Degrees().value());
-	frc::SmartDashboard::PutNumber(m_name + "/Angle", getAngle());
-	#endif
+	// frc::SmartDashboard::PutNumber(m_name + "/Speed", getSpeed());
 	// frc::SmartDashboard::PutNumber(m_name + "/Target", m_state.angle.Degrees().value());
 	// frc::SmartDashboard::PutNumber(m_name + "/Angle", getAngle());
 }
