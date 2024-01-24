eb3a1ffe3af8108c9f6125c97f5e0d457ba60c82
Luis Leyva <luisoleyvan@gmail.com>
2024-01-24 15:19:33 -0600

    Changed rotation speed

--- src/main/Commands/Climbing/Climbing.cpp
+++ src/main/Commands/Climbing/Climbing.cpp
@@ -4,8 +4,8 @@ frc2::CommandPtr Climb() {
 	auto path = pathplanner::PathPlannerPath::fromPathFile("AMP Climb");
 
 	pathplanner::PathConstraints constraints = pathplanner::PathConstraints(
-		3.0_mps, 4.0_mps_sq,
-		540_deg_per_s, 720_deg_per_s_sq);
+		3.0_mps, 3.0_mps_sq,
+		180_deg_per_s, 180_deg_per_s_sq);
 
 	return pathplanner::AutoBuilder::pathfindThenFollowPath(
 		path,
