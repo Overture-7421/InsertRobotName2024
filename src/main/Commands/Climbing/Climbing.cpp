33b236b81ded94f5fe08a7d59352716923c7597e
Luis Leyva <luisoleyvan@gmail.com>
2024-01-25 20:57:11 -0600

    New pathfinding speeds

--- src/main/Commands/Climbing/Climbing.cpp
+++ src/main/Commands/Climbing/Climbing.cpp
@@ -10,11 +10,11 @@ ClimbingLocation findClosestClimbingLocation(Chassis* chassis) {
 	auto alliance = frc::DriverStation::GetAlliance();
 	auto shouldFlip = alliance.has_value() && (alliance.value() == frc::DriverStation::Alliance::kRed);
 
-	for(auto location : climbingLocations) {
-		if(shouldFlip){
-			distancesToClimbingLocations.push_back(std::pair{location.first, chassis->getOdometry().Translation().Distance(pathplanner::GeometryUtil::flipFieldPosition(location.second))});
+	for (auto location : climbingLocations) {
+		if (shouldFlip) {
+			distancesToClimbingLocations.push_back(std::pair{ location.first, chassis->getOdometry().Translation().Distance(pathplanner::GeometryUtil::flipFieldPosition(location.second)) });
 		} else {
-			distancesToClimbingLocations.push_back(std::pair{location.first, chassis->getOdometry().Translation().Distance(location.second)});
+			distancesToClimbingLocations.push_back(std::pair{ location.first, chassis->getOdometry().Translation().Distance(location.second) });
 		}
 	}
 
@@ -29,24 +29,22 @@ frc2::CommandPtr Climb(Chassis* chassis) {
 	std::shared_ptr<pathplanner::PathPlannerPath> climbPathBack = pathplanner::PathPlannerPath::fromPathFile("AMP Climb Back");
 
 	pathplanner::PathConstraints constraints = pathplanner::PathConstraints(
-		4.0_mps, 2.0_mps_sq,
+		2.0_mps, 1.0_mps_sq,
 		560_deg_per_s, 720_deg_per_s_sq);
 
 	return frc2::cmd::Select<ClimbingLocation>([chassis]() {return findClosestClimbingLocation(chassis);},
-		std::pair{ClimbingLocation::Left, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathLeft, constraints)},
-		std::pair{ClimbingLocation::Right, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathRight, constraints)},
-		std::pair{ClimbingLocation::Back, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathBack, constraints)}
+		std::pair{ ClimbingLocation::Left, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathLeft, constraints) },
+		std::pair{ ClimbingLocation::Right, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathRight, constraints) },
+		std::pair{ ClimbingLocation::Back, pathplanner::AutoBuilder::pathfindThenFollowPath(climbPathBack, constraints) }
 	);
 };
 
-std::ostream& operator<< (std::ostream& os, ClimbingLocation location)
-{
-    switch (location)
-    {
-        case ClimbingLocation::Left : return os << "Left" ;
-        case ClimbingLocation::Right: return os << "Right";
-        case ClimbingLocation::Back: return os << "Back";
-        // omit default case to trigger compiler warning for missing cases
-    };
-    return os << static_cast<std::uint16_t>(location);
+std::ostream& operator<< (std::ostream& os, ClimbingLocation location) {
+	switch (location) {
+	case ClimbingLocation::Left: return os << "Left";
+	case ClimbingLocation::Right: return os << "Right";
+	case ClimbingLocation::Back: return os << "Back";
+		// omit default case to trigger compiler warning for missing cases
+	};
+	return os << static_cast<std::uint16_t>(location);
 }
