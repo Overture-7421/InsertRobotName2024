e7d91c212ffacef91d6dbaf72d44b3608617ea98
Luis Leyva <luisoleyvan@gmail.com>
2024-01-26 18:07:38 -0600

    Added support arms to climb

--- src/main/Commands/Climbing/Climbing.cpp
+++ src/main/Commands/Climbing/Climbing.cpp
@@ -3,6 +3,7 @@
 #include <pathplanner/lib/auto/NamedCommands.h>
 
 #include "main/Commands/SuperStructureMoveByDistance/SuperStructureMoveByDistance.h"
+#include "main/Commands/SupportArmsMoveByDistance/SupportArmsMoveByDistance.h"
 
 #include <iostream>
 
@@ -16,7 +17,7 @@ frc::Pose2d flipPoseIfNeeded(frc::Pose2d pose) {
 	return pose;
 }
 
-units::length::meter_t getDistanceToChassis(Chassis* chassis, frc::Pose2d targetPose){
+units::length::meter_t getDistanceToChassis(Chassis* chassis, frc::Pose2d targetPose) {
 	return chassis->getOdometry().Translation().Distance(flipPoseIfNeeded(targetPose).Translation());
 }
 
@@ -26,22 +27,31 @@ ClimbingLocation findClosestClimbingLocation(Chassis* chassis) {
 
 
 	for (auto location : climbingLocations) {
-		distancesToClimbingLocations.push_back(std::pair{ location.first, getDistanceToChassis(chassis, location.second)});
+		distancesToClimbingLocations.push_back(std::pair{ location.first, getDistanceToChassis(chassis, location.second) });
 	}
 
 	std::sort(distancesToClimbingLocations.begin(), distancesToClimbingLocations.end(), [](auto a, auto b) { return a.second < b.second;});
 	return distancesToClimbingLocations.front().first;
 }
 
-frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure) {
+frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure, SupportArms* supportArms) {
 
-	SuperStructureState startingState{-4, -100 };
+	SuperStructureState startingState{ -4, -100 };
 	SuperStructureState targetState{ 90, -90 };
 
 	SuperStructureMoveByDistance::Profile profile;
 	profile.profileActivationDistance = 1_m;
 	profile.startingState = startingState;
 	profile.targetState = targetState;
+
+	SupportArmsState startingState2{ 0 };
+	SupportArmsState targetState2{ 90 };
+
+	SupportArmsMoveByDistance::Profile profile2;
+	profile2.profileActivationDistance = 1_m;
+	profile2.startingState = startingState2;
+	profile2.targetState = targetState2;
+
 
 	std::shared_ptr<pathplanner::PathPlannerPath> climbPathLeft = pathplanner::PathPlannerPath::fromPathFile("AMP Climb Left");
 	std::shared_ptr<pathplanner::PathPlannerPath> climbPathRight = pathplanner::PathPlannerPath::fromPathFile("AMP Climb Right");
@@ -58,7 +68,8 @@ frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure) {
 			frc2::cmd::Wait(3_s),
 			frc2::cmd::Parallel(
 				pathplanner::AutoBuilder::followPath(climbPathLeft),
-				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[0].second);}).ToPtr()
+				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[0].second);}).ToPtr(),
+				SupportArmsMoveByDistance(supportArms, profile2, [=]() {return getDistanceToChassis(chassis, climbingLocations[0].second);}).ToPtr()
 			)
 		) },
 		std::pair{ ClimbingLocation::Right, frc2::cmd::Sequence(
@@ -67,16 +78,18 @@ frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure) {
 			frc2::cmd::Wait(3_s),
 			frc2::cmd::Parallel(
 				pathplanner::AutoBuilder::followPath(climbPathRight),
-				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[1].second);}).ToPtr()
+				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[1].second);}).ToPtr(),
+				SupportArmsMoveByDistance(supportArms, profile2, [=]() {return getDistanceToChassis(chassis, climbingLocations[1].second);}).ToPtr()
 			)
-		)  },
+		) },
 		std::pair{ ClimbingLocation::Back, frc2::cmd::Sequence(
 			pathplanner::AutoBuilder::pathfindToPose(flipPoseIfNeeded(climbPathBack->getStartingDifferentialPose()), constraints),
 			frc2::cmd::RunOnce([=]() {superStructure->setTargetCoord(startingState);}, {superStructure}),
 			frc2::cmd::Wait(3_s),
 			frc2::cmd::Parallel(
 				pathplanner::AutoBuilder::followPath(climbPathBack),
-				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[2].second);}).ToPtr()
+				SuperStructureMoveByDistance(superStructure, profile, [=]() {return getDistanceToChassis(chassis, climbingLocations[2].second);}).ToPtr(),
+				SupportArmsMoveByDistance(supportArms, profile2, [=]() {return getDistanceToChassis(chassis, climbingLocations[2].second);}).ToPtr()
 			)
 		) }
 	);
--- src/main/Commands/Climbing/Climbing.h
+++ src/main/Commands/Climbing/Climbing.h
@@ -7,19 +7,19 @@
 #include <vector>
 #include <utility>
 
-enum class ClimbingLocation { 
-    Left,
-    Right,
-    Back
+enum class ClimbingLocation {
+	Left,
+	Right,
+	Back
 };
 
 std::ostream& operator<< (std::ostream& os, ClimbingLocation location);
 
-const std::vector<std::pair<ClimbingLocation, frc::Pose2d>> climbingLocations {
-    {ClimbingLocation::Left, {{4.52_m, 4.67_m}, {120_deg}}},
-    {ClimbingLocation::Right,{{4.57_m, 3.51_m}, {-120_deg}}},
-    {ClimbingLocation::Back, {{5.51_m, 4.10_m}, {0_deg}}}
+const std::vector<std::pair<ClimbingLocation, frc::Pose2d>> climbingLocations{
+	{ClimbingLocation::Left, {{4.52_m, 4.67_m}, {120_deg}}},
+	{ClimbingLocation::Right,{{4.57_m, 3.51_m}, {-120_deg}}},
+	{ClimbingLocation::Back, {{5.51_m, 4.10_m}, {0_deg}}}
 };
 
-frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure);
+frc2::CommandPtr Climb(Chassis* chassis, SuperStructure* superStructure, SupportArms* supportArms);
 
