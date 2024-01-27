a64b2d52f7254a84c0dc68f191aaa8ee780672f0
Luis Leyva <luisoleyvan@gmail.com>
2024-01-26 18:20:59 -0600

    Inverted Motor

--- src/main/Subsystems/SupportArms/SupportArms.h
+++ src/main/Subsystems/SupportArms/SupportArms.h
@@ -35,7 +35,7 @@ private:
 
 
 	// LowerMotors
-	OverTalonFX m_lowerRight{ 23, ControllerNeutralMode::Brake, false, "rio" };
+	OverTalonFX m_lowerRight{ 23, ControllerNeutralMode::Brake, true, "rio" };
 
 	// State
 	SupportArmsState m_TargetState{ getCurrentState() };
