9c41849239f33b06a1b8cf971f129bcafb5e0144
DiegoArmandoFondon <a01199394@tec.mx>
2024-01-30 18:31:01 -0600

    Composed Commands Tests

---
+++ src/main/Commands/AmpCommand/AmpCommand.cpp
@@ -0,0 +1,11 @@
+#include "AmpCommand.h"
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.
+// For more information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+AmpCommand::AmpCommand(SuperStructure* superStucture) {
+
+  AddCommands(
+
+  );
+}
---
+++ src/main/Commands/SourceGrabCommand/SourceGrabCommand.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "SourceGrabCommand.h"
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.
+// For more information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+SourceGrabCommand::SourceGrabCommand(SuperStructure* superStructure, Storage* storage) {
+
+  AddCommands(
+    SuperStructureCommand(superStructure, {30.0, 30.0}),
+    StorageCommand(storage, 3_V)
+  );
+}
