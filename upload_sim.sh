8fb878da874f96099c824bce0e2a10eb685e3b9a
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2024-02-02 14:11:54 -0600

    Fix regex expression to correctly ignore some folders

--- upload_sim.sh
+++ upload_sim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-FILES=$(ls -1a | grep -iEv 'build|ctre_sim|.git|.gradle|.gitignore|\.$')
+FILES=$(ls -1a | grep -iEv '^build$|^ctre_sim$|^.git$|^.gradle$|^.gitignore$|\.$')
 echo $FILES
 scp -r $FILES overture@photonvision.local:~/Documents/Gits/InsertRobotName2024
