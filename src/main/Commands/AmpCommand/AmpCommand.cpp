38de6e8abd8991e72c431e98aabb25221d44f21d
Luis Leyva <luisoleyvan@gmail.com>
2024-02-05 14:42:05 -0600

    AnotherUpdateVisionCommands

--- src/main/Commands/AmpCommand/AmpCommand.cpp
+++ src/main/Commands/AmpCommand/AmpCommand.cpp
@@ -7,7 +7,7 @@ AmpCommand::AmpCommand(SuperStructure* superStucture, Shooter* shooter, Storage*
 
 	AddCommands(
 		frc2::ParallelCommandGroup(
-			SuperStructureCommand(superStucture, { 70.0, -0.0 }),
+			SuperStructureCommand(superStucture, { 60.0, -10.0 }),
 			ShooterCommand(shooter, 6.00)
 		)
 	);
