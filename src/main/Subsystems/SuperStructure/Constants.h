8b7b879d8f1d4c87aef1717b51178cab465f5f2a
Luis Leyva <luisoleyvan@gmail.com>
2024-03-22 19:17:21 -0600

    New reduction

--- src/main/Subsystems/SuperStructure/Constants.h
+++ src/main/Subsystems/SuperStructure/Constants.h
@@ -2,7 +2,7 @@
 #include "SuperStructureState.h"
 
 namespace SuperStructureConstants {
-	const static double LowerAngleGearRatio = 230.4;
+	const static double LowerAngleGearRatio = 172.8;
 	const static double UpperAngleGearRatio = 90.0;
 
 	const static double LowerAngleLowerLimit = -32.5;
@@ -14,7 +14,7 @@ namespace SuperStructureConstants {
 	const static double LowerAngleSafetyThreshold = -25;
 	const static double UpperAngleSafetyLimit = -70;
 
-	const static SuperStructureState GroundGrabState = { LowerAngleLowerLimit, -1.0 };
+	const static SuperStructureState GroundGrabState = { LowerAngleLowerLimit, UpperAngleUpperLimit };
 	const static SuperStructureState SourceGrabState = { LowerAngleLowerLimit, -0.35 };
 	const static SuperStructureState ManualSpeakerState = { -12.0, -35.0 };
 	const static SuperStructureState AmpState = { 65.0, -30.0 };
