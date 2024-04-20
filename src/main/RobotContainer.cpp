cc18681a524415bcbd08951d7a1af59b2aafd237
Luis Leyva <luisoleyvan@gmail.com>
2024-04-19 20:40:40 -0600

    Add 0.1 timeout to auto shoot

--- src/main/RobotContainer.cpp
+++ src/main/RobotContainer.cpp
@@ -18,7 +18,7 @@ RobotContainer::RobotContainer() {
 
 	pathplanner::NamedCommands::registerCommand("ClosedCommand", std::move(ClosedCommand(&superStructure, &intake, &storage, &shooter).ToPtr()));
 	pathplanner::NamedCommands::registerCommand("VisionSpeakerCommand", std::move(frc2::cmd::Sequence(
-		VisionSpeakerCommandNoShoot(&chassis, &superStructure, &shooter, &targetProvider).ToPtr().WithTimeout(0.25_s),
+		VisionSpeakerCommandNoShoot(&chassis, &superStructure, &shooter, &targetProvider).ToPtr().WithTimeout(0.35_s),
 		VisionSpeakerCommand(&chassis, &superStructure, &shooter, &targetProvider, &storage).ToPtr()
 	)));
 	pathplanner::NamedCommands::registerCommand("VisionShootNoDelay", std::move(VisionSpeakerCommand(&chassis, &superStructure, &shooter, &targetProvider, &storage).ToPtr()));
